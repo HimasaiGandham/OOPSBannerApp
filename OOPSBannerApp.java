6f2c5cb0e518cfc0b937b1cd15dbc35495aa3aaa
HimasaiGandham <himasaigandham277@gmail.com>
2026-03-03 22:11:18 +0530

    UC1: Print OOPS implemented

---
+++ OOPSBannerApp.java
@@ -0,0 +1,20 @@
+/**
+ * OOPSBannerApp
+ * Prints the text "OOPS" to the console.
+ * 
+ * @author HimaSai
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    /**
+     * Main method - entry point of the application
+     */
+    public static void main(String[] args) {
+
+        // Print OOPS banner
+        System.out.println("OOPS");
+
+    }
+}
